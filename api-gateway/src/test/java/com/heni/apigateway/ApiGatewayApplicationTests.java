048e137e0728d7f62baaccada47d4f3103f8bda9
heni-ouerfelli <132569078+Heni2002@users.noreply.github.com>
2024-03-03 03:04:02 +0100

    Microservices implementation

---
+++ api-gateway/src/test/java/com/heni/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heni.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ config-server/src/test/java/com/heni/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heni.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
